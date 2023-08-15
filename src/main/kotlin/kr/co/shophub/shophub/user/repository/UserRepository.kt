4eafcedcb6baddd4733e3afafab6439e907a499d
Taaate <an981022@naver.com>
2023-08-15 15:31:46 +0900

    feat: 유저 리포지토리 구현

---
+++ src/main/kotlin/kr/co/shophub/shophub/user/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package kr.co.shophub.shophub.user.repository
+
+import kr.co.shophub.shophub.user.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+}
