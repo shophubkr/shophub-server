0b5b072b7cee6b1962752fe711cd5015bf8f9795
Taaate <an981022@naver.com>
2023-08-15 16:11:59 +0900

    feat: 유저 기본 정보 구현

---
+++ src/main/kotlin/kr/co/shophub/shophub/user/domain/User.kt
@@ -0,0 +1,17 @@
+package kr.co.shophub.shophub.user.domain
+
+import jakarta.persistence.*
+
+@Entity
+class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    var id: Long = 0L,
+
+    val email: String,
+    val password: String,
+    val nickname: String,
+    val providerId: String,
+    val profile: String,
+    val isDeleted: Boolean,
+)
