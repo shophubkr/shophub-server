e54a6fefc8308388b735d24d73735fd144fa28f1
Taaate <an981022@naver.com>
2023-08-20 13:59:03 +0900

    fix: 로그인 실패시 응답 JSON 형식으로 반환

--- src/main/kotlin/kr/co/shophub/shophub/global/login/handler/LoginFailureHandler.kt
+++ src/main/kotlin/kr/co/shophub/shophub/global/login/handler/LoginFailureHandler.kt
@@ -1,11 +1,15 @@
 package kr.co.shophub.shophub.global.login.handler
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+import java.time.LocalDateTime
 
-class LoginFailureHandler : SimpleUrlAuthenticationFailureHandler() {
+class LoginFailureHandler(
+    private val objectMapper: ObjectMapper,
+) : SimpleUrlAuthenticationFailureHandler() {
 
     override fun onAuthenticationFailure(
         request: HttpServletRequest,
@@ -14,8 +18,12 @@ class LoginFailureHandler : SimpleUrlAuthenticationFailureHandler() {
     ) {
         response.status = HttpServletResponse.SC_BAD_REQUEST
         response.characterEncoding = "UTF-8"
-        response.contentType = "text/plain;charset=UTF-8"
-        response.writer.write("로그인 실패! 이메일이나 비밀번호를 확인해주세요.")
+        response.contentType = "application/json;charset=UTF-8"
+        val data = mutableMapOf<String, Any>()
+        data["timeStamp"] = LocalDateTime.now().toString().substring(0,19)
+        data["message"] = "로그인에 실패 했습니다! 이메일이나 비밀번호를 확인하세요."
+        response.writer.println(objectMapper.writeValueAsString(data))
     }
 
 }
+
