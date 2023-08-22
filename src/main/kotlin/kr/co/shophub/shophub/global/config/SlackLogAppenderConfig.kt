470c1c3d897ba975b699fe58cd0a1cf67fb4ae66
Jang DaeYoung <789456jang@naver.com>
2023-08-23 03:57:23 +0900

    fix: 실행 환경 조건 추가

--- src/main/kotlin/kr/co/shophub/shophub/global/config/SlackLogAppenderConfig.kt
+++ src/main/kotlin/kr/co/shophub/shophub/global/config/SlackLogAppenderConfig.kt
@@ -4,8 +4,10 @@ import net.gpedro.integrations.slack.SlackApi
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("ec2-dev")
 class SlackLogAppenderConfig {
 
     @Value("\${logging.slack.webhook-uri}")
