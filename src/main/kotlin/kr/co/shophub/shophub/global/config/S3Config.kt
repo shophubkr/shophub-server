06bcb8a2b035eed556e57e56383a4e520d36c333
Jang DaeYoung <789456jang@naver.com>
2023-09-22 22:52:05 +0900

    chore: @Profile 삭제 및 CI에서 application-s3.yml가 주입되도록 변경

--- src/main/kotlin/kr/co/shophub/shophub/global/config/S3Config.kt
+++ src/main/kotlin/kr/co/shophub/shophub/global/config/S3Config.kt
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("ec2-dev")
 class S3Config {
 
     @Value("\${cloud.aws.credentials.access-key}")
