a575d5c86c247c15476c5611959a423fb2b35c90
Taaate <an981022@naver.com>
2023-09-26 21:47:03 +0900

    fix: 인증 실패 처리 수정

--- src/main/kotlin/kr/co/shophub/shophub/global/oauth/handler/OAuth2LoginFailureHandler.kt
+++ src/main/kotlin/kr/co/shophub/shophub/global/oauth/handler/OAuth2LoginFailureHandler.kt
@@ -1,7 +1,12 @@
 package kr.co.shophub.shophub.global.oauth.handler
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import kr.co.shophub.shophub.global.dto.CustomProblemDetail
+import kr.co.shophub.shophub.global.error.UnauthenticatedException
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.authentication.AuthenticationFailureHandler
 import org.springframework.stereotype.Component
@@ -10,12 +15,29 @@ import org.springframework.stereotype.Component
  * 삭제해도 될 것 같은데 일단 뒀습니다.
  */
 @Component
-class OAuth2LoginFailureHandler : AuthenticationFailureHandler {
+class OAuth2LoginFailureHandler(
+    private val objectMapper: ObjectMapper
+) : AuthenticationFailureHandler {
     override fun onAuthenticationFailure(
         request: HttpServletRequest,
         response: HttpServletResponse,
         exception: AuthenticationException
     ) {
-        throw exception
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "utf-8"
+        response.status = HttpStatus.UNAUTHORIZED.value()
+
+        val problemDetail = CustomProblemDetail.forStatusAndDetail(
+            HttpStatus.UNAUTHORIZED,
+            UnauthenticatedException("인증에 실패했습니다.").message!!
+        )
+
+        val errorBody = mapOf(
+            "error" to problemDetail
+        )
+
+        val body = objectMapper.writeValueAsString(errorBody)
+        response.writer.write(body)
+
     }
 }
