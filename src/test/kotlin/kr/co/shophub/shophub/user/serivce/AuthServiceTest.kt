e99041601364a37d901f78a45a795a4f7a49bee8
Taaate <an981022@naver.com>
2023-10-03 21:49:43 +0900

    test: 유저 도메인 테스트 작성

--- src/test/kotlin/kr/co/shophub/shophub/user/serivce/AuthServiceTest.kt
+++ src/test/kotlin/kr/co/shophub/shophub/user/serivce/AuthServiceTest.kt
@@ -191,7 +191,7 @@ class AuthServiceTest : BehaviorSpec({
                     authService.reIssueToken(refreshToken)
                 }.message
 
-                message shouldBe "유저가 존재 하지 않습니다."
+                message shouldBe "유저를 찾을 수 없습니다."
             }
         }
     }
