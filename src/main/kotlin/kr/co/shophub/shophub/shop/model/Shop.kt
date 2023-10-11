697bb574507713864e6f02e934facaffb7b6b4d2
Taaate <an981022@naver.com>
2023-10-11 17:59:00 +0900

    feat: 매장 등록 시점에 사업자 정보 같이 받도록 구현

--- src/main/kotlin/kr/co/shophub/shophub/shop/model/Shop.kt
+++ src/main/kotlin/kr/co/shophub/shophub/shop/model/Shop.kt
@@ -3,6 +3,7 @@ package kr.co.shophub.shophub.shop.model
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
+import kr.co.shophub.shophub.business.model.Business
 import kr.co.shophub.shophub.global.model.BaseEntity
 import kr.co.shophub.shophub.product.model.product.Product
 import kr.co.shophub.shophub.shop.dto.ChangeShopRequest
@@ -45,6 +46,9 @@ class Shop(
 
     @OneToMany(mappedBy = "shop", cascade = [CascadeType.ALL], orphanRemoval = true)
     var tags: MutableList<ShopTag> = mutableListOf(),
+
+    @OneToOne(mappedBy = "shop", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var business: Business? = null,
 
     @Column(name = "is_deleted")
     private var deleted: Boolean = false
@@ -98,5 +102,9 @@ class Shop(
     fun cancelFollow() {
         this.followCnt--
         this.level = followCnt/10 + 1
+    }
+
+    fun addBusiness(savedBusiness: Business) {
+        this.business = savedBusiness
     }
 }
