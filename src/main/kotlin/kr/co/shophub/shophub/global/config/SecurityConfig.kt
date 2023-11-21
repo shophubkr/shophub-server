ba1d060e0741abedcda19af392c61fbcea8b5796
조기천 <55054505+sectionr0@users.noreply.github.com>
2023-11-21 12:59:27 +0900

    #41 - CORS 설정 추가 (#42)

--- src/main/kotlin/kr/co/shophub/shophub/global/config/SecurityConfig.kt
+++ src/main/kotlin/kr/co/shophub/shophub/global/config/SecurityConfig.kt
@@ -8,8 +8,10 @@ import kr.co.shophub.shophub.global.jwt.service.JwtService
 import kr.co.shophub.shophub.global.login.service.LoginService
 import kr.co.shophub.shophub.global.oauth.handler.OAuth2LoginSuccessHandler
 import kr.co.shophub.shophub.global.oauth.service.CustomOAuth2UserService
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
@@ -24,6 +26,10 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @Configuration
 @EnableWebSecurity
---
+++ src/main/kotlin/kr/co/shophub/shophub/global/config/WebConfig.kt
@@ -0,0 +1,25 @@
+package kr.co.shophub.shophub.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    companion object {
+        private const val ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH"
+        private const val ALLOWED_HEADER_NAMES = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    }
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://localhost:8081",
+                "http://13.209.100.56",
+                "http://13.209.100.56:8081",
+            )
+            .allowedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+            .exposedHeaders("Authorization", "RefreshToken")
+    }
+}
