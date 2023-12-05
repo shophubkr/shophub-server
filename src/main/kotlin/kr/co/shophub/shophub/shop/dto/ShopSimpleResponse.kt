ee18a34da8d5695d71a7e17bb94738c8dc8a4548
sectionr0 <sectionr0@gmail.com>
2023-12-05 20:13:54 +0900

    feat: Shop response 값 추가

--- src/main/kotlin/kr/co/shophub/shophub/shop/dto/ShopSimpleResponse.kt
+++ src/main/kotlin/kr/co/shophub/shophub/shop/dto/ShopSimpleResponse.kt
@@ -4,16 +4,20 @@ import kr.co.shophub.shophub.shop.model.Shop
 
 data class ShopSimpleResponse(
     val id: Long,
+    val image: String,
     val name: String,
     val address: String,
     val introduce: String,
+    val minPrice: Int,
     val checkCoupon: Boolean,
-){
+) {
     constructor(shop: Shop) : this(
         id = shop.id,
+        image = shop.images[0].imgUrl,
         name = shop.name,
         address = shop.address,
         introduce = shop.introduce,
+        minPrice = shop.products.minByOrNull { it.price }?.price ?: 0,
         checkCoupon = true
     )
 }
